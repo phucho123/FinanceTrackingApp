af4b665effe7e35cfbf5f8c955689fcf0f015bac
Phuc Ho <hotrongphuc643@gmail.com>
2024-05-31 21:18:26 +0700

    update

--- frontend/config.js
+++ frontend/config.js
@@ -1,2 +1,3 @@
-const localhost = "192.168.1.9";
-export const apiBaseUrl = `http://${localhost}:5001/v1/api`;
+// const localhost = "192.168.1.9";
+// export const apiBaseUrl = `http://${localhost}:5001/v1/api`;
+export const apiBaseUrl = "https://appbackend-ar2t.onrender.com/v1/api"
